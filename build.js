d93821a3cfc097a3deb402f9906eb0096d92029d
Sysco <159677029+Syyysco@users.noreply.github.com>
2025-10-07 01:20:39 +0200

    Upload required files for README.md and package.json for npm build

--- build.js
+++ build.js
@@ -1,3 +1,24 @@
+/**
+ * © 2025 Typerflip. All rights reserved.
+ * 
+ * This software is provided under a proprietary license.
+ * You may use this software for personal and educational purposes only.
+ * Commercial use, modification, and redistribution are strictly prohibited
+ * without explicit written permission.
+ * 
+ * For licensing inquiries: syscodev@proton.me
+ * License violations will be prosecuted to the full extent of the law.
+ * 
+ * AUTHOR: Jose Fco. López (aka Sysco)
+ *      Github:     [ https://github.com/Syyysco ] @Syyysco
+ *      Portfolio:  [ https://syyysco.github.io ]
+ * 
+ * APP: Typerflip - Social editor:
+ *      Repo:       [ https://github.com/Syyysco/Typerflip ]
+ *      Web:        [ https://Syyysco.github.io/Typerflip ]
+ * 
+ */
+
 import * as esbuild from 'esbuild';
 import { config } from './js/build/config.js';
 import { createTree } from './js/build/utils.js';
